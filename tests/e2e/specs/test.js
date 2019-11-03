ec45e506a48fb093f671cec34cdb4c6813cba8eb
Cristina Grim <cristinaponce.designer@gmail.com>
2019-11-03 20:18:29 +0100

    dummy test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,6 +1,6 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Visit main route', () => {
   it('Visits the app root url', () => {
     cy.visit('/');
     cy.contains('h1', 'My Videogames List');
